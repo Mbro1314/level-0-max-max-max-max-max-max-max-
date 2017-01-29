61801dbe27261c6f94ea9f26ac3b14a6a85566d8
melissapc <league@WTS8.attlocal.net>
2017-01-28 17:34:31 -0800

    dose not work

---
+++ src/org/jointheleague/graphical/robot/PRAC.java
@@ -0,0 +1,73 @@
+package org.jointheleague.graphical.robot;
+
+import javax.swing.JOptionPane;
+
+public class PRAC {
+	if(d <= 97&& d >= 97){
+		JOptionPane.showMessageDialog(null, "+A" );}
+		if(d <= 100 && d >= 100){
+			JOptionPane.showMessageDialog(null, "+A "+ " good job" );}
+			if(d <= 98&& d >= 98){
+				JOptionPane.showMessageDialog(null, "+A" );}
+				if(d <= 99&& d >= 99){
+					JOptionPane.showMessageDialog(null, "+A" );}
+					if(d <= 96&& d >= 96){
+						JOptionPane.showMessageDialog(null, "A" );}
+						if(d <= 95&& d >= 95){
+							JOptionPane.showMessageDialog(null, "A" );}
+							if(d <= 94&& d >= 94){
+								JOptionPane.showMessageDialog(null, "A" );}
+								if(d <= 93&& d >= 93){
+									JOptionPane.showMessageDialog(null, "A" );}
+								if(d <= 92&& d >= 92){
+									JOptionPane.showMessageDialog(null, "-A" );}
+								if(d <= 91&& d >= 91){
+									JOptionPane.showMessageDialog(null, "-A" );}
+								if(d <= 90&& d >= 90){
+										JOptionPane.showMessageDialog(null, "-A" );}
+								if(d <= 89&& d >= 89){
+											JOptionPane.showMessageDialog(null, "+B" );}
+								if(d <= 88&& d >= 88){
+									JOptionPane.showMessageDialog(null, "+B" );}
+								if(d <= 87 && d >= 87){
+									JOptionPane.showMessageDialog(null, "+B" );}
+								if(d <= 86&& d >= 86){
+									JOptionPane.showMessageDialog(null, "B" );}
+								if(d <= 85&& d >= 85){
+									JOptionPane.showMessageDialog(null, "B" );}
+								if(d <= 84&& d >= 84){
+									JOptionPane.showMessageDialog(null, "B" );}
+								if(d <= 83&& d >= 83){
+									JOptionPane.showMessageDialog(null, "B" );}
+								if(d <= 82&& d >= 82){
+									JOptionPane.showMessageDialog(null, "-B" );}
+								if(d <= 81&& d >= 81){
+									JOptionPane.showMessageDialog(null, "-B" );}
+								if(d <= 80 && d >= 80){
+										JOptionPane.showMessageDialog(null, "-B" );}
+								if(d <= 79){
+									JOptionPane.showMessageDialog(null, "+C" );}
+								if(d <= 78 && d >= 79){
+									JOptionPane.showMessageDialog(null, "+C" );}
+								if(d <= 77&& d >= 77){
+									JOptionPane.showMessageDialog(null, "+C" );}
+								if(d <= 76&& d >= 76){
+									JOptionPane.showMessageDialog(null, "C" );}
+								if(d <= 75&& d >= 75){
+									JOptionPane.showMessageDialog(null, "C" );}
+								if(d <= 74 && d >= 74){
+									JOptionPane.showMessageDialog(null, "C" );}
+								if(d <= 73 && d >= 73){
+									JOptionPane.showMessageDialog(null, "C" );}
+								if(d <= 72&& d >= 72){
+									JOptionPane.showMessageDialog(null, "+D" );}
+								if(d <= 71&& d >= 71){
+									JOptionPane.showMessageDialog(null, "+D" );}
+								if(d <= 70&& d >= 70){
+									JOptionPane.showMessageDialog(null, "+D" );}
+								if(d <= 69 && d >= 67){
+										JOptionPane.showMessageDialog(null, "+D" );}
+								if(d <= 66 && d >= 63 ){
+									JOptionPane.showMessageDialog(null, "-C" );}
+}
+}
---
+++ src/org/jointheleague/graphical/robot/grade.java
@@ -0,0 +1,72 @@
+package org.jointheleague.graphical.robot;
+
+import javax.swing.JOptionPane;
+
+public class grade {
+	public static void main(String[] args) {
+	
+double d = Double.parseDouble("80");
+		
+		String answer = JOptionPane.showInputDialog("what grade?");
+	JOptionPane.showInputDialog(answer);
+
+	if(d <= 100 ){
+		JOptionPane.showMessageDialog(null, "+A" );}
+	else if(d >= 99 ){
+		JOptionPane.showMessageDialog(null, "+A" );}
+	else if(d >= 98 ){
+		JOptionPane.showMessageDialog(null, "+A" );}
+	else if(d >= 97 ){
+		JOptionPane.showMessageDialog(null, "+A" );}
+	else if(d >= 96 ){
+		JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 94 ){
+		JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 93 ){
+			JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 92 ){
+		JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 91 ){
+		JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 90 ){
+		JOptionPane.showMessageDialog(null, "A" );}
+	else if(d >= 89 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 88 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 87 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 86 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 85 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 84 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 83 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 82 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	else if(d >= 82 ){
+		JOptionPane.showMessageDialog(null, "B" );}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
+	}
+	
+	
+
+	
+
